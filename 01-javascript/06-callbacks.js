474fe8d691e846d01dd2fafaa2605d1a06dd90a7
Lesly Tipanluiza <lesly.tipanluiza@epn.edu.ec>
2021-12-01 07:11:57 -0500

    Callbacks JS

---
+++ 01-javascript/06-callbacks.js
@@ -0,0 +1,43 @@
+// 06-callbacks.js
+const fs = require('fs'); // file system
+//console.log('PRIMERO');
+// 06-06-ejemplo.txt -> Hola
+
+fs.readFile(
+    './06-ejemplo.txt',
+    'utf-8',
+    (error, contenido) =>{
+        if(error){
+            console.error({mensaje:'error leyendo contenido', error:error});
+        }else {
+            console.log(contenido)
+
+        }
+    }
+);
+// console.log('TERCERO');
+
+// fs.readFile(
+//    './01-variables.js' ,
+//     'utf-8',
+//     (error, contenido1) =>{
+//         if(error){
+//             console.error({mensaje:'error leyendo contenido 1', error:error});
+//         }else {
+//             //console.log(contenido)
+//             fs.readFile(
+//                 './06-ejemplo.txt' ,
+//                 'utf-8',
+//                 (error2, contenido2) =>{
+//                     if(error2){
+//                         console.error({mensaje:'error leyendo contenido 2', error:error2});
+//                     }else {
+//                         console.log(contenido1, contenido2);
+//
+//                     }
+//                 }
+//             );
+//
+//         }
+//     }
+// );
