50d049aeb1efedf6c46e1123be7059b3eeb38a44
Lesly Tipanluiza <lesly.tipanluiza@epn.edu.ec>
2022-02-07 21:14:20 -0500

    03 - Angular

--- 03-angular/ejemplo/src/app/app.component.ts
+++ 03-angular/ejemplo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {WebsocketsService} from "./servicios/websockets/websockets.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemplo';
+
+  constructor(private readonly websocketsService:WebsocketsService) {
+  }
+
+  eventoHola(){
+    this.websocketsService.ejecutarEventoHola()
+  }
 }
